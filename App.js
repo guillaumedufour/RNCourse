8149b915c618907eb07c3fff1aaa552e9b2e0e9f
Guillaume Gdlab <guillaume@gdlab.fr>
2025-10-28 14:22:44 +0100

    suite

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {useState} from "react";
+import {StyleSheet, Button, View, TextInput, SafeAreaView, Text, FlatList} from 'react-native';
 
 export default function App() {
+  const [inputValue, setInputValue] = useState("")
+  const [inputResult, setInputResult] = useState([])
+  const onPressBtn = () => {
+    setInputResult((prevState) => [...prevState, inputValue]);
+    setInputValue("")
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={[styles.container]}>
+      <View style={styles.inputContainer}>
+        <TextInput value={inputValue}
+                   onChangeText={(text) => setInputValue(text)}
+                   style={styles.input}/>
+        <Button title='Valider' onPress={onPressBtn}/>
+      </View>
+      <View style={styles.resultContainer}>
+        <FlatList data={inputResult} renderItem={({item}) => {
+          return (
+            <View style={styles.itemContainer}><Text style={styles.item}>{item}</Text></View>
+          )
+        }}/>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -15,6 +32,33 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  inputContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    paddingHorizontal: 8
+  },
+  input: {
+    width: '80%',
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderColor: 'grey',
+  },
+  resultContainer: {
+    flex: 1,
+    width: '100%',
+    padding: 8
+  },
+  itemContainer: {
+    width: '100%',
+    height: 38,
+    marginVertical: 12,
+    borderRadius: 8,
+    backgroundColor: 'rgba(0,0,0,0.75)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  item: {
+    color: 'white'
+  }
 });
