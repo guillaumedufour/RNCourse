7e14a24e85b1e2ae469137c0b85f8465ab3b1321
Guillaume GD LAB <guillaume@gdlab.fr>
2025-10-30 17:01:51 +0100

    ajout d'une modale pour ajouter les élements

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import {useState} from "react";
-import {StyleSheet, Button, View, TextInput, SafeAreaView, Text, FlatList} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  SafeAreaView,
+  Text,
+  FlatList,
+  Modal,
+  TouchableOpacity,
+  TextInput
+} from 'react-native';
 
 export default function App() {
   const [inputValue, setInputValue] = useState("")
   const [inputResult, setInputResult] = useState([])
-  const onPressBtn = () => {
-    setInputResult((prevState) => [...prevState, inputValue]);
-    setInputValue("")
+  const [isModalVisible, setModalVisible] = useState(false);
+  const onOpenModal = () => {
+    setModalVisible(true);
+  }
+  const onCloseModal = () => {
+    setModalVisible(false);
+  }
+
+  const onCreateItem = (item) => {
+    setInputResult(prev => [...prev, inputValue]);
+    setInputValue("");
+    setModalVisible(false);
   }
   return (
     <SafeAreaView style={[styles.container]}>
       <View style={styles.inputContainer}>
-        <TextInput value={inputValue}
-                   onChangeText={(text) => setInputValue(text)}
-                   style={styles.input}/>
-        <Button title='Valider' onPress={onPressBtn}/>
+        <TouchableOpacity activeOpacity={0.9} style={styles.btn} onPress={onOpenModal}>
+          <Text style={styles.btnText}>Ajouter un élement</Text>
+        </TouchableOpacity>
       </View>
       <View style={styles.resultContainer}>
         <FlatList data={inputResult} renderItem={({item}) => {
@@ -23,6 +40,19 @@ export default function App() {
           )
         }}/>
       </View>
+      <Modal visible={isModalVisible} animationType={"slide"}>
+        <View style={styles.modalView}>
+          <TextInput value={inputValue} onChangeText={setInputValue} style={styles.input}/>
+          <View style={styles.modalBtnContainer}>
+            <TouchableOpacity activeOpacity={0.9} style={[styles.modalBtns, styles.createBtn]} onPress={onCreateItem}>
+              <Text style={styles.modalBtnText}>Créer</Text>
+            </TouchableOpacity>
+            <TouchableOpacity activeOpacity={0.9} style={[styles.modalBtns, styles.closeBtn]} onPress={onCloseModal}>
+              <Text style={styles.modalBtnText}>Fermer</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </SafeAreaView>
   );
 }
@@ -39,10 +69,42 @@ const styles = StyleSheet.create({
     paddingHorizontal: 8
   },
   input: {
-    width: '80%',
+    width: '100%',
     backgroundColor: 'white',
     borderWidth: 1,
     borderColor: 'grey',
+    borderRadius: 8,
+    height: 42,
+    fontSize: 20,
+    paddingHorizontal: 8,
+    paddingVertical: 4
+  },
+  btn: {
+    width: '100%',
+    height: 50,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'violet',
+    marginTop: 50
+  },
+  modalBtns: {
+    width: '40%',
+    height: 50,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 16,
+  },
+  btnText: {
+    fontSize: 20,
+    color: 'white',
+  },
+  createBtn: {
+    backgroundColor: 'blue',
+  },
+  closeBtn: {
+    backgroundColor: 'black',
   },
   resultContainer: {
     flex: 1,
@@ -59,6 +121,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   item: {
+    fontSize: 20,
     color: 'white'
+  },
+  modalView: {
+    flex: 1,
+    backgroundColor: 'grey',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 24
+  },
+  modalBtnContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+  },
+  modalBtnText: {
+    color: 'white',
+    fontSize: 20
   }
 });
